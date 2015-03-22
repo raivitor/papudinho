f119dca5415514c8feca1903b3ececa2110c90f2
Raí Vitor <raivitor@gmail.com>
2015-03-22 15:50:43 -0300

    Usuario tem acesso aos seus dados e pode alterar sua senha.

--- www/js/app.js
+++ www/js/app.js
@@ -90,6 +90,15 @@ app.config(function($stateProvider, $urlRouterProvider) {
       }
     })
 
+    .state('eventmenu.dados', {
+      url: "/dados",
+      views: {
+        'menuContent' :{
+          templateUrl: "dados"
+        }
+      }
+    })
+
     .state('addCartao', {
       url: '/addCartao',
       templateUrl: 'addCartao'
@@ -630,4 +639,83 @@ app.config(function($stateProvider, $urlRouterProvider) {
       $scope.sair = function() {
         window.localStorage['login'] = 0;
       }
+    }]);
+
+    app.controller('dados', ['$scope', '$http',  function($scope, $http) {
+      $scope.msg = ""
+      $scope.submitDados = function() {
+
+        if($scope.passwordOld == undefined ){
+          $scope.msg = "O campo 'Senha Antiga' está vazio";
+          return 0;
+        } 
+
+        if($scope.passwordOld == undefined ){
+          $scope.msg = "'Senha Antiga' precisa ter 8 ou mais dígitos";
+          return 0;
+        } 
+
+        if($scope.password == undefined ){
+          $scope.msg = "O campo 'Senha Nova' está vazio";
+          return 0;
+        } 
+
+        if($scope.password2 == undefined ){
+          $scope.msg = "O campo 'Confirmar Senha Nova' está vazio";
+          return 0;
+        } 
+        
+        if($scope.password.length < 8 ){
+          $scope.msg = "'Senha Nova' precisa ter 8 ou mais dígitos";
+          return 0;
+        }
+
+        if($scope.password != $scope.password2){
+          $scope.msg = "Senhas diferentes";
+          return 0;
+        }
+
+        $scope.msg = '';
+
+        $http({
+          url: 'http://developer-papudinho.herokuapp.com/webservice/change_password', 
+          method: "POST",
+          params: {
+            id: window.localStorage['id'],
+            old_password: $scope.passwordOld,
+            new_password: $scope.password
+          }
+        }).
+
+        success(function (data, status, headers, config) {
+          $scope.msg = "Senha alterada com sucesso";
+          $scope.password = '';
+          $scope.passwordOld = '';
+          $scope.password2 = '';
+        }).
+
+        error(function (data, status, headers, config) {
+          $scope.msg = "Senha antiga está errada";
+          console.log('Error add Amigo');
+        });
+
+        return 0; 
+      }
+
+
+      function timedCount() {
+        var time;
+        if(window.localStorage['login'] == 1){
+          clearTimeout(time);
+          $scope.email = window.localStorage['email'];
+          $scope.nome = window.localStorage['name'];
+
+          return 0;
+        }
+        else{
+          time = setTimeout(function(){ timedCount() }, 500);
+        }
+      }
+
+      timedCount();
     }]);
