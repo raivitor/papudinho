70c647468404441bc93602358ac27ed6c192a882
Raí Vitor <raivitor@gmail.com>
2015-05-20 00:45:48 -0300

    alteração do bg, retirada do botão de foto, add loading no reset senha

--- www/js/Ctrl_NovaSenha.js
+++ www/js/Ctrl_NovaSenha.js
@@ -1,11 +1,13 @@
 app.controller('NovaSenha', ['$scope', '$http', '$location', '$ionicPopup',  function($scope, $http, $location, $ionicPopup) {
+  $scope.msg = " ";
+  $scope.checked = false;
   $scope.submitsenha = function() {
     if($scope.email == undefined ){
       $scope.msg = "O campo 'Email' está vazio";
       return 0;
     } 
     $scope.msg = "";
-    
+    $scope.checked = true;
     $http({
       url: 'http://developer-papudinho.herokuapp.com/webservice/reset_password/', 
       method: "POST",
@@ -15,11 +17,13 @@ app.controller('NovaSenha', ['$scope', '$http', '$location', '$ionicPopup',  fun
     }).
 
     success(function (data, status, headers, config) {
+      $scope.checked = false;
       alerta($ionicPopup, "Notificação", "Enviamos a nova senha para o seu email.");
       $location.path('home');  
     }).
 
     error(function (data, status, headers, config) {
+      $scope.checked = false;
       $scope.msg = "Erro ao enviar a nova senha, tente novamente mais tarde.";
       console.log('Error NovaSenha');
     });
