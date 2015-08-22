8d6ff48a296430b24f957d6eae619b4e5ca12b66
Raí Vitor <raivitor@gmail.com>
2015-08-21 23:13:19 -0300

    Correção das configurações

--- www/js/Ctrl_Config.js
+++ www/js/Ctrl_Config.js
@@ -1,3 +1,20 @@
 app.controller('Config', ['$scope',  function($scope) {
-  
+	/*
+	console.log(window.localStorage['gps']);
+	window.localStorage['gps'] = 1;
+	gps = window.localStorage['gps'];
+  	$scope.promocoes = { checked: window.localStorage['promotion'] == 0 };
+  	$scope.gps = { checked: gps.valueOf() };
+  	$scope.visibilidade = { checked: window.localStorage['visibility'] == true };
+	*/
+  	$scope.NotificationChange = function(id) {
+  		switch (id){
+  			case 1:
+  				break;
+  			case 2:
+  				break;
+  			case 3:
+  				break;
+  		}
+  	};
 }]);
