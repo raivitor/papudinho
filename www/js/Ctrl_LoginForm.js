369d82dc9d69f07faebc0daf7438b0f38af609db
Raí Vitor <raivitor@gmail.com>
2015-08-21 23:15:03 -0300

    Correção da tela de configurações

--- www/js/Ctrl_LoginForm.js
+++ www/js/Ctrl_LoginForm.js
@@ -41,6 +41,9 @@ app.controller('LoginForm', ['$scope', '$http', '$location', function($scope, $h
       window.localStorage['email'] = data.email;
       window.localStorage['name'] = data.name;
       window.localStorage['phone'] = data.phone;
+      window.localStorage['promotion'] = data.promotion;
+      window.localStorage['gps'] = data.gps
+      window.localStorage['visibility'] = data.visibility;
       $location.path('/menu/home');  
     }).
 
