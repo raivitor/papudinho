fc40b32a19858baf69c93a5fc4330a7f29cb0370
Rai Vitor Morais da Silva <raivitor@gmail.com>
2015-08-26 17:52:35 -0300

    Mascaras

--- www/js/Ctrl_CadastroForm.js
+++ www/js/Ctrl_CadastroForm.js
@@ -3,66 +3,54 @@ app.controller('CadastroForm', ['$scope', '$http', '$location', '$ionicPopup',  
   $scope.submitcadastro = function() {
     $scope.msg = "";
     if($scope.user == undefined ){
-      $scope.msg = "O campo 'Nome' está vazio";
+      $scope.msg = "O campo 'Nome' está vazio.";
       return 0;
     }
 
     if($scope.email == undefined ){
-      $scope.msg = "O campo 'Email' está vazio";
+      $scope.msg = "O campo 'Email' está vazio.";
       return 0;
     } 
-
+    
     if($scope.phone == undefined ){
-      $scope.msg = "O campo 'Celular' está vazio";
+      $scope.msg = "O campo 'Celular' está vazio.";
+      return 0;
+    }
+
+    if($scope.phone.length != 16){
+      $scope.msg = "Insira o DDD junto ao seu número.";
       return 0;
     }
 
     if($scope.cpf == undefined ){
-      $scope.msg = "O campo 'CPF' está vazio";
+      $scope.msg = "O campo 'CPF' está vazio.";
       return 0;
     }
 
-    if($scope.cpf.length != 11 ){
-      $scope.msg = "CPF inválido, utilize somente números";
+    if($scope.cpf.length != 14 ){
+      $scope.msg = "CPF inválido.";
       return 0;
     }
 
     if($scope.password == undefined ){
-      $scope.msg = "O campo 'Senha' está vazio";
+      $scope.msg = "O campo 'Senha' está vazio.";
       return 0;
     } 
 
     if($scope.password2 == undefined ){
-      $scope.msg = "O campo 'Confirmar Senha' está vazio";
+      $scope.msg = "O campo 'Confirmar Senha' está vazio.";
       return 0;
     } 
     
     if($scope.password.length < 8 ){
-      $scope.msg = "Senha precisa ter 8 ou mais dígitos";
+      $scope.msg = "Senha precisa ter 8 ou mais dígitos.";
       return 0;
-    }
-
-    if($scope.phone.length < 10){
-      $scope.msg = "Insira o DDD junto ao seu número";
-      return 0;
-    }
+    }    
 
     if($scope.password != $scope.password2){
       $scope.msg = "Senhas diferentes";
       return 0;
     }
-
-    cpf = "";
-    for(i = 0; i<11; i++){
-      if(i==9){
-        cpf = cpf.concat("-");
-      }
-      else if(i%3==0 && i!=0){
-        cpf = cpf.concat(".");
-      }
-      cpf = cpf.concat($scope.cpf[i]);
-    }
-    
 
     $scope.msg = "";
     $scope.checked = true;
@@ -73,7 +61,7 @@ app.controller('CadastroForm', ['$scope', '$http', '$location', '$ionicPopup',  
         name: $scope.user,
         email: $scope.email,
         phone: $scope.phone,
-        cpf: cpf,
+        cpf: $scope.cpf,
         password: $scope.password,
         password_confirmation: $scope.password2
       }
--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('app', ['ionic', 'ngCordova', 'ui.mask']);
+var app = angular.module('app', ['ionic', 'ngCordova', 'ngMask']);
 
 app.run(function($ionicPlatform, $ionicHistory) {
   $ionicPlatform.ready(function() {
