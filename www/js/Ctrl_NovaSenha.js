773e6899a04d57b41f199dae62e7d104ff26dbf3
Raí Vitor <raivitor@gmail.com>
2015-08-27 23:42:36 -0300

    Log nova senha

--- www/js/Ctrl_NovaSenha.js
+++ www/js/Ctrl_NovaSenha.js
@@ -1,12 +1,10 @@
 app.controller('NovaSenha', ['$scope', '$http', '$location', '$ionicPopup',  function($scope, $http, $location, $ionicPopup) {
-  $scope.msg = " ";
   $scope.checked = false;
   $scope.submitsenha = function() {
     if($scope.email == undefined ){
       $scope.msg = "O campo 'Email' está vazio";
       return 0;
     } 
-    $scope.msg = "";
     $scope.checked = true;
     $http({
       url: 'http://developer-papudinho.herokuapp.com/webservice/reset_password/', 
@@ -24,8 +22,12 @@ app.controller('NovaSenha', ['$scope', '$http', '$location', '$ionicPopup',  fun
 
     error(function (data, status, headers, config) {
       $scope.checked = false;
-      $scope.msg = "Erro ao enviar a nova senha, tente novamente mais tarde.";
-      console.log('Error NovaSenha: '+data);
+      alerta($ionicPopup, "Notificação", "Erro ao enviar a nova senha, tente novamente mais tarde.");
+      console.log('Error NovaSenha');
+      console.log("Data: "+data);
+      console.log("Status: "+status);
+      console.log("headers: "+headers);
+      console.log("Config: "+config);
     });
   }
 }]);
