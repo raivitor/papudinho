a22ce6269f35723f5d9a3b51bb85c3af9d950fce
Raí Vitor <raivitor@gmail.com>
2015-09-10 00:02:10 -0300

    Chat

--- www/js/Ctrl_Chat.js
+++ www/js/Ctrl_Chat.js
@@ -1,32 +1,20 @@
-app.controller('Chat', ['$scope', '$stateParams',  function($scope, $stateParams) {
-
-var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  },{
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
-  console.log(chats[1]);
-  $scope.msg = 1;
-  $scope.chat = chats[1];
+app.controller('Chat', ['$scope', '$stateParams', '$firebaseArray',  function($scope, $stateParams, $firebaseArray) {
+  $scope.meuName = G_usuario.id;
+  $scope.nameAmigo = $stateParams.chatId;
+  var ref;
+  if(G_usuario.id < $stateParams.chatId){
+    ref = new Firebase("https://chat-prot.firebaseio.com/"+G_usuario.id+""+$stateParams.chatId);
+  } else{
+    ref = new Firebase("https://chat-prot.firebaseio.com/"+$stateParams.chatId+""+G_usuario.id);
+  }
+   
+  $scope.messages = $firebaseArray(ref);
+  $scope.addMessage = function() {
+    console.log($scope.messages);
+    $scope.messages.$add({
+      name: G_usuario.id,
+      text: $scope.message
+    });
+     $scope.message = "";
+  };
 }]);
