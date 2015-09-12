0615989083a919d54b69e6c6d0e630c37a8aa000
Raí Vitor <raivitor@gmail.com>
2015-09-12 00:20:03 -0300

    inicio da correção do scroll

--- www/js/Ctrl_Chat.js
+++ www/js/Ctrl_Chat.js
@@ -1,20 +1,26 @@
-app.controller('Chat', ['$scope', '$stateParams', '$firebaseArray',  function($scope, $stateParams, $firebaseArray) {
+app.controller('Chat', ['$scope', '$stateParams', '$firebaseArray', '$ionicScrollDelegate', function($scope, $stateParams, $firebaseArray, $ionicScrollDelegate) {
   $scope.meuName = G_usuario.id;
   $scope.nameAmigo = $stateParams.chatId;
   var ref;
   if(G_usuario.id < $stateParams.chatId){
     ref = new Firebase("https://chat-prot.firebaseio.com/"+G_usuario.id+""+$stateParams.chatId);
+    console.log("https://chat-prot.firebaseio.com/"+G_usuario.id+""+$stateParams.chatI);
   } else{
     ref = new Firebase("https://chat-prot.firebaseio.com/"+$stateParams.chatId+""+G_usuario.id);
   }
-   
+
   $scope.messages = $firebaseArray(ref);
+
+  //console.log($scope.messages.length);
+
   $scope.addMessage = function() {
-    console.log($scope.messages);
     $scope.messages.$add({
       name: G_usuario.id,
       text: $scope.message
     });
      $scope.message = "";
   };
+
+ // $ionicScrollDelegate.scrollBottom();
+
 }]);
