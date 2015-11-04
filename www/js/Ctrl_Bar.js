8f318a60fc9cda469af5a1205bbc1ac8907d1c49
Raí Vitor <raivitor@gmail.com>
2015-11-03 23:12:30 -0300

    abrir link do site em um brower

--- www/js/Ctrl_Bar.js
+++ www/js/Ctrl_Bar.js
@@ -1,3 +1,13 @@
 app.controller('Bar', ['$scope', '$stateParams',  function($scope, $stateParams) {
 	$scope.bar = G_bares.getId($stateParams.id);
+
+	$scope.link = function(site){
+		var ref = window.open('http://www.'+site, '_blank', 'location=yes');
+		var myCallback = function() { 
+
+		}
+		ref.addEventListener('loadstart', myCallback);
+		//ref.removeEventListener('loadstart', myCallback);
+		//window.open(site, '_blank', 'location=yes');
+	}
 }]);
