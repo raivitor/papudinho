7e173682f3c2d0333430d8a938e826de52d4fea1
Raí Vitor <raivitor@gmail.com>
2016-01-08 13:53:43 -0300

    icone de loading

--- www/js/Ctrl_MeusCartoes.js
+++ www/js/Ctrl_MeusCartoes.js
@@ -1,15 +1,18 @@
 app.controller('MeusCartoes', ['$scope', '$http', '$interval', 'CartoesPessoais', function($scope, $http, $interval, CartoesPessoais) {
-
-  $interval(atualizar, 800, false);
+  $scope.checked = true;
+  $interval(atualizar, 2000, false);
   function atualizar(){
     $scope.cartoes = CartoesPessoais.getCartoes(G_usuario.id);
     $scope.cartoes = $scope.cartoes.data;
     $scope.msg = "";
+    
     if($scope.cartoes == false){
       $scope.msg = "Nenhum cartão cadastrado no momento";
+      $scope.checked = false;
     }
     else if($scope.cartoes == -1){
       $scope.msg = "Erro no servidor";
+      $scope.checked = false;
     }
   }
   atualizar();
