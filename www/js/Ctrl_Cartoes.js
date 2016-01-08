92278f33aa5513304dfa960d0a484fe7bddb0ace
Raí Vitor <raivitor@gmail.com>
2016-01-08 14:02:03 -0300

    Correção da tela de cartões

--- www/js/Ctrl_Cartoes.js
+++ www/js/Ctrl_Cartoes.js
@@ -1,34 +1,33 @@
-app.controller('Cartoes', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
+app.controller('Cartoes', ['$scope', '$http', function($scope, $http) {
   atualizar();
-  $interval(atualizar, 2000, false);
-  //$scope.configUser = true;
+  //$interval(atualizar, 2000, false);
   function atualizar(){
-    if(window.localStorage['atualizarCartao'] == 1){
-      window.localStorage['atualizarCartao'] = 0;
-      $http({
-        url: 'http://developer-papudinho.herokuapp.com/webservice/cards/', 
-        method: "GET",
-        params: {
-          user: G_usuario.id
-        }
-      }).
+    $http({
+      url: 'http://developer-papudinho.herokuapp.com/webservice/cards/', 
+      method: "GET",
+      params: {
+        user: G_usuario.id
+      }
+    }).
 
-      success(function (data, status, headers, config) {
-        $scope.$broadcast('scroll.refreshComplete');
-        $scope.Cartoes = data;
-        $scope.msg = " ";
-        console.log(data);
-        if(data == 0){
-          $scope.msg = "Ainda não tem cartão em nenhum bar!";
-        }
-      }).
-
-      error(function (data, status, headers, config) {
-        $scope.$broadcast('scroll.refreshComplete');
-        console.log('Error cartoes');
-      });
-    } else{
+    success(function (data, status, headers, config) {
       $scope.$broadcast('scroll.refreshComplete');
-    }
+      $scope.Cartoes = data;
+      $scope.msg = " ";
+      console.log(data);
+      if(data == 0){
+        $scope.msg = "Ainda não tem cartão em nenhum bar!";
+      }
+    }).
+
+    error(function (data, status, headers, config) {
+      $scope.$broadcast('scroll.refreshComplete');
+      console.log('Error cartoes');
+    });
+    $scope.$broadcast('scroll.refreshComplete');
+  }
+
+  $scope.doRefresh = function() {
+    atualizar();
   }
 }]);
