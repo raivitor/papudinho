40438b8542df2d07af6a893b15ce8acaf97e691f
Mayron Cachina <mayroncachina@gmail.com>
2016-06-02 14:38:36 -0300

    dados do bar

--- www/js/Ctrl_Bares.js
+++ www/js/Ctrl_Bares.js
@@ -1,4 +1,4 @@
-app.controller('Bares', ['$scope', '$http',  function($scope, $http) {
+app.controller('Bares', ['$scope', '$http','$ionicModal' , function($scope, $http,$ionicModal) {
   $http({
     url: 'http://developer-papudinho.herokuapp.com/webservice/bars', 
     method: "GET"
@@ -12,4 +12,36 @@ app.controller('Bares', ['$scope', '$http',  function($scope, $http) {
   error(function (data, status, headers, config) {
     console.log('Error bares');
   });
+
+
+$scope.modal = function(bar){
+
+  console.log(bar)
+  $scope.bar = bar;
+  $scope.oModal1.show();
+}
+
+  $ionicModal.fromTemplateUrl('bar-modal.html', {
+      id: '1', 
+      scope: $scope,
+      backdropClickToClose: false,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.oModal1 = modal;
+    });
+
+
+  $scope.$on('modal.shown', function(event, modal) {
+      console.log('Modal ' + modal.id + ' is shown!');
+    });
+
+  $scope.$on('modal.hidden', function(event, modal) {
+    console.log('Modal ' + modal.id + ' is hidden!');
+  });
+
+  $scope.$on('$destroy', function() {
+    console.log('Destroying modals...');
+    $scope.oModal1.remove();
+  });    
+
 }]);
