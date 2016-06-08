a0a6f334c37ea3a8feeee84da5c1931040918d5f
Mayron Cachina <mayroncachina@gmail.com>
2016-06-07 21:52:16 -0300

    logoff

--- www/js/Ctrl_Sidebar.js
+++ www/js/Ctrl_Sidebar.js
@@ -1,11 +1,33 @@
 app.controller('Sidebar', ['$scope', '$state', function($scope, $state) {
   $scope.sair = function() {
+
+    var idUser = window.localStorage['login'];
+
     delete(window.localStorage['login']);
     delete(window.localStorage['LastImgCard']);
     delete(window.localStorage['LastImgDrink']);
     window.localStorage['login'] = NULL;
     window.localStorage['LastImgCard'] = NULL;
     window.localStorage['LastImgDrink'] = NULL;
+
+    var req = {
+        method: 'POST',
+        url: 'http://developer-papudinho.herokuapp.com/webservice/device_token',
+        params:{
+          id: idUser, 
+          token: ""
+        }
+      }
+      $http(req).
+        then(function (sucesso) {
+          /*console.warn("Sucesso token");
+          console.log(sucesso);*/
+        },
+        function(fail){
+          console.error("Fail token");
+          //console.log(fail);
+        });
+
     // depois pesquiser isso
     //$state.transitionTo("login", {}, {reload: true});
     $state.go('login', {}, { reload: true });
