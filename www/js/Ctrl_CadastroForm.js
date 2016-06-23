a2a3b0879776577fc757bd60bb46384c29683791
Jhony Lucas <jhonyjl37@gmail.com>
2016-06-23 09:10:39 -0300

    verificando se ao cadastrar usuário nome ou email estão vazios

--- www/js/Ctrl_CadastroForm.js
+++ www/js/Ctrl_CadastroForm.js
@@ -1,16 +1,22 @@
 app.controller('CadastroForm', ['$scope', '$http', '$location', '$ionicPopup', '$ionicModal', function($scope, $http, $location, $ionicPopup, $ionicModal) {
   $scope.checked = false;
+
+  function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+  }
+
   $scope.submitcadastro = function() {
     $scope.msg = "";
-    if($scope.user == undefined ){
+    if(isBlank($scope.user)){
       $scope.msg = "O campo 'Nome' está vazio.";
       return 0;
     }
-    if($scope.email == undefined || $scope.email == ""){
+
+    if(isBlank($scope.email)){
       $scope.msg = "O campo 'Email' está vazio.";
       return 0;
-    } 
-    
+    }
+
     if($scope.phone == undefined ){
       $scope.msg = "O campo 'Celular' está vazio.";
       return 0;
@@ -35,17 +41,17 @@ app.controller('CadastroForm', ['$scope', '$http', '$location', '$ionicPopup', '
     if($scope.password == undefined ){
       $scope.msg = "O campo 'Senha' está vazio.";
       return 0;
-    } 
+    }
 
     if($scope.password2 == undefined ){
       $scope.msg = "O campo 'Confirmar Senha' está vazio.";
       return 0;
-    } 
-    
+    }
+
     if($scope.password.length < 8 ){
       $scope.msg = "Senha precisa ter 8 ou mais dígitos.";
       return 0;
-    }    
+    }
 
     if($scope.password != $scope.password2){
       $scope.msg = "Senhas diferentes";
@@ -55,7 +61,7 @@ app.controller('CadastroForm', ['$scope', '$http', '$location', '$ionicPopup', '
     $scope.msg = "";
     $scope.checked = true;
     $http({
-      url: servidor+'/webservice/registering_user/', 
+      url: servidor+'/webservice/registering_user/',
       method: "POST",
       params: {
         name: $scope.user,
@@ -77,7 +83,7 @@ app.controller('CadastroForm', ['$scope', '$http', '$location', '$ionicPopup', '
       window.localStorage['login'] = 1;
       G_usuario = data;
       $scope.checked = false;
-      $location.path('/menu/home');  
+      $location.path('/menu/home');
     }).
 
     error(function (data, status, headers, config) {
